1cf294e03d0446cbcded7454ffe65216b3b7551d
Leandro López <lelopez031189@gmail.com>
2022-05-11 17:58:12 -0300

    modificacion de App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import React, { Component } from 'react'
-import {BrowserRouter, Routes, Route } from 'react-router-dom';
-import {Home} from "./components/Home"
-import {AddProducts} from "./components/AddProducts"
-import { ProductsContextProvider} from '.global/ProductsContext';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home/Home";
+import ProductDetail from "./pages/Detail/ProductDetail";
+import Category from "./components/Categorys/Category";
+import Message404 from "./components/Message-404/message404";
+import Store from "./pages/Store/Store";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+import Cart from "./components/Cart/Cart";
+import { SiteProvider } from "./context/SiteContext";
 
-export class App extends Component {
-  render() {
-    return (
-      <ProductsContextProvider> 
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element= {<Home/>} />
-          <Route exact path="addproducts" element= {<AddProducts/>} />
-        </Routes>
-      </BrowserRouter>
-      </ProductsContextProvider>
-    )
-  }
-}
+const App = () => {
+  return (
+    <SiteProvider>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/store" component={Store} />
+            <Route path="/cart" component={Cart} />
+            <Route path="/:category/:id" component={ProductDetail} />
+            <Route path="/:category" component={Category} />
+            <Route path="*" component={Message404} />
+          </Switch>
+          <Footer copyright="Todos los derechos reservados." />
+        </div>
+      </Router>
+    </SiteProvider>
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import "bootstrap/dist/css/bootstrap.css";
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
-    <App />
-
-);
-
-
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
