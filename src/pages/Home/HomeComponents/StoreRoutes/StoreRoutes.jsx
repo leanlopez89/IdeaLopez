61a5c6f9f438d609cc7719ddb1f2f8f9627e1191
Leandro López <lelopez031189@gmail.com>
2022-05-18 18:40:15 -0300

    Resolviendo conflictos

--- src/pages/Home/HomeComponents/StoreRoutes/StoreRoutes.jsx
+++ src/pages/Home/HomeComponents/StoreRoutes/StoreRoutes.jsx
@@ -9,52 +9,52 @@ const StoreRoutes = () => {
       id="productos"
     >
       <Link
-        to="/lamparas"
+        to="/salud"
         className="col-10 col-sm-5 col-lg-2 img-link text-center mx-3 my-3"
       >
         <img
           src="https://firebasestorage.googleapis.com/v0/b/buncits-41df0.appspot.com/o/RoutesArtboard-1-copy-5.png?alt=media&token=efb3990e-0aad-495f-949b-ff888c731b9e"
-          alt="ruta hacia lámparas"
+          alt="ruta hacia salud"
           className="img-fluid w-100"
         />
       </Link>
       <Link
-        to="/sofas"
+        to="/alimentos"
         className="col-10 col-sm-5 col-lg-2 img-link text-center mx-3 my-3"
       >
         <img
           src="https://firebasestorage.googleapis.com/v0/b/buncits-41df0.appspot.com/o/RoutesArtboard-1.png?alt=media&token=664a34a6-884a-4fff-9420-a12e20806e6d"
-          alt="ruta hacia sofás"
+          alt="ruta hacia alimentos"
           className="img-fluid w-100"
         />
       </Link>
       <Link
-        to="/estanterias"
+        to="/juguetes"
         className="col-10 col-sm-5 col-lg-2 img-link text-center mx-3 my-3"
       >
         <img
           src="https://firebasestorage.googleapis.com/v0/b/buncits-41df0.appspot.com/o/RoutesArtboard-1-copy-3.png?alt=media&token=a7e04e66-d5fa-4929-8074-388ec94ece67"
-          alt="ruta hacia estanterías"
+          alt="ruta hacia juguetes"
           className="img-fluid w-100"
         />
       </Link>
       <Link
-        to="/sillas"
+        to="/accesorios"
         className="col-10 col-sm-5 col-lg-2 img-link text-center mx-3 my-3"
       >
         <img
           src="https://firebasestorage.googleapis.com/v0/b/buncits-41df0.appspot.com/o/RoutesArtboard-1-copy-2.png?alt=media&token=e0bf6098-f97d-4132-af1d-bbafc1ed9dac"
-          alt="ruta hacia sillas"
+          alt="ruta hacia accesorios"
           className="img-fluid w-100"
         />
       </Link>
       <Link
-        to="/gabinetes"
+        to="/camas"
         className="col-10 col-sm-5 col-lg-2 img-link text-center mx-3 my-3"
       >
         <img
           src="https://firebasestorage.googleapis.com/v0/b/buncits-41df0.appspot.com/o/RoutesArtboard-1-copy-4.png?alt=media&token=16e63127-a465-45f5-b0a7-79e31b89ef16"
-          alt="ruta hacia gabinetes"
+          alt="ruta hacia camas"
           className="img-fluid w-100"
         />
       </Link>
