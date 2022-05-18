32f395601be9a8914e20261cf5b0901161325850
Leandro López <lelopez031189@gmail.com>
2022-05-18 18:44:14 -0300

    Agregando Firebase

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,13 +2,14 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-var firebaseConfig = {
-  apiKey: "AIzaSyC-fgKG5DVlM1bcdnCSFGooZRcnnaDoXx0",
-  authDomain: "buncits-41df0.firebaseapp.com",
-  projectId: "buncits-41df0",
-  storageBucket: "buncits-41df0.appspot.com",
-  messagingSenderId: "1000442280075",
-  appId: "1:1000442280075:web:2386ed9af7d05059287c8e",
+const firebaseConfig = {
+  apiKey: "AIzaSyBFnCAk8Kd3uYu-bKUAopGxaLZLWKVMUu0",
+  authDomain: "reactecommerce-7710e.firebaseapp.com",
+  projectId: "reactecommerce-7710e",
+  storageBucket: "reactecommerce-7710e.appspot.com",
+  messagingSenderId: "102683451252",
+  appId: "1:102683451252:web:bf75b3e921d32820ee5621",
+  measurementId: "G-CZR9DH09L6"
 };
 
 
